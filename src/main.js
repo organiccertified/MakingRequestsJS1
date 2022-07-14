885b53524da8622b3aae86f5c4e47511d3271f85
Erick M <erick.michelr@gmail.com>
2022-07-14 01:16:11 -0500

    doubts

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const BASE_URL = "http://localhost:5000";
 function index() {
     return axios
     .get(`${BASE_URL}/constellations`)
-    .then(({data}) =>  data)
+    .then(({data}) =>  {return data})  //returning ALL data
     .catch(({ message }) => {
       console.error(message);
     });
@@ -14,7 +14,7 @@ function index() {
 function create(body) {
     return axios
     .post(`${BASE_URL}/constellations`, body)
-    .then(({ data }) => data)
+    .then(({ data }) => data)  //THINK I MISS TO FINISH THIS?
     .catch(({ message }) => {
       console.error(message);
     });
@@ -23,7 +23,7 @@ function create(body) {
 function show(id) {
     return axios
     .get(`${BASE_URL}/constellations/${id}`)
-    .then(({data}) => {
+    .then(({data}) => {               //THINK I MISS TO FINISH THIS?
       return data;
     })
     .catch(({ message }) => {
@@ -34,7 +34,7 @@ function show(id) {
 function update(id, body) {
   return axios
   .put(`${BASE_URL}/constellations/${id}`)
-  .then(({ data }) => data)
+  .then(({ data }) => data)                       //THINK I MISS TO FINISH THIS?
   .catch(({ message }) => {
     console.error(message);
   });
@@ -43,7 +43,12 @@ function update(id, body) {
 function destroy(id) {
   return axios
   .delete(`${BASE_URL}/constellations/${id}`)
-  .then(({ data }) => data)
+  .then(({ data }) => {                            //THINK I MISS TO FINISH THIS?
+    let result = data.find((data)=> { 
+      data.id == id
+    } )    
+  return result
+})
   .catch(({ message }) => {
     console.error(message);
   });
